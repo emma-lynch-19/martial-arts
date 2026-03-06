3e49d5e55da819275fec220b7f6e183b6b751b44
emma-lynch-19 <emmalynch2701@gmail.com>
2026-03-06 10:19:54 +0000

    Update and rename .eleventy.js to eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,4 @@
-module.exports = function(eleventyConfig) {
+export default function (eleventyConfig) {
 
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return new Date(dateObj).toLocaleDateString("en-GB", {
@@ -8,4 +8,4 @@ module.exports = function(eleventyConfig) {
     });
   });
 
-};
+}
